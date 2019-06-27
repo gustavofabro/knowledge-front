89f69d26675da6beb08c07d4ae55e02f88fa6084
Gustavo <gustavofabro.f@gmail.com>
2019-06-26 21:46:03 -0300

    toggle menu

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import 'font-awesome/css/font-awesome.css'
 
 import App from './App'
+import store from './config/store'
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App)
 }).$mount('#app')
