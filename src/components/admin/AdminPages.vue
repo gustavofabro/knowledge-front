214c03454139c2045db8b038f5de412b935a49e3
Gustavo <gustavofabro.f@gmail.com>
2019-07-04 21:34:08 -0300

    routers

---
+++ src/components/admin/AdminPages.vue
@@ -0,0 +1,19 @@
+<template> 
+    <dic class="admin-pages">
+        <PageTitle icon="fa fa-cogs" main="Administração de sistemas"
+            sub="Cadastros & Cia" />
+    </dic>
+</template>
+
+<script>
+import PageTitle from '../template/PageTItle'
+
+export default {
+    name: 'AdminPages',
+    components: { PageTitle }
+}
+</script>
+
+<style>
+
+</style>
