082ee2590a4442c9bf6336a7d6a5d9a12385e1ee
Gustavo <gustavofabro.f@gmail.com>
2019-07-11 21:55:32 -0300

    statics

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+// Remover
+
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ikd1c3Rhdm8gRmFicm8iLCJlbWFpbCI6Imd1c3Rhdm9Ac21va2luZ3NuYWtlcy5jb20uYnIiLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTYyODkyMDY0LCJleHAiOjE1NjMxNTEyNjR9.WQL2IxY3qHLzd__dpSuElJf5Oh6OusXu-DDXLn-Enuw'
+
 new Vue({
   store,
   router,
