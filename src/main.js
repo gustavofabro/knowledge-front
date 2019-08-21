b8f272d6a6e4c6deda64b0703092cd23b9435549
Gustavo <gustavofabro.f@gmail.com>
2019-08-20 22:07:21 -0300

    admin permissions

--- src/main.js
+++ src/main.js
@@ -9,10 +9,6 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
-// Remover
-
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ikd1c3Rhdm8gRmFicm8iLCJlbWFpbCI6Imd1c3Rhdm9Ac21va2luZ3NuYWtlcy5jb20uYnIiLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTY1NzQxNzQwLCJleHAiOjE1NjYwMDA5NDB9.vplhMW4VXBu9y3Lwxm5wfUkjhsZ6DjU6qubmTo63WLE'
-
 new Vue({
   store,
   router,
